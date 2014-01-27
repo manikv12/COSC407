6cd9a2d69dbd84204dc087c73b6ecc9e59ffbe69
ethan <ethan@phantasm>
2014-01-27 13:01:13 -0800

    Assignment 4

--- lab4/countsort.c
+++ lab4/countsort.c
@@ -1,26 +1,67 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <omp.h>
+
+#define ARR_SIZE 10000
+#define NUM_THR 32
+
 void countsort();
 int main(int argc, char *argv[])
 {
-    int a[] = {5,7,3,8,9,23,28734,37,3,48,51,1,9,54};
-    countsort(a, 14);
+    int i;
+    int arr[ARR_SIZE];
+    srand(getpid());
+    #pragma omp parallel for num_threads(NUM_THR)
+    for(i = 0;i < ARR_SIZE; i++)
+       arr[i] = rand()%ARR_SIZE; 
+    
+    #pragma omp barrier
+    float iTime = omp_get_wtime();
+    countsort(&arr, ARR_SIZE);
+    float fTime = omp_get_wtime() - iTime;
+    //for(i = 0; i<ARR_SIZE; i++) printf("%d, ", arr[i]);
+    printf("Time to sort in Parallel: %f\n", fTime);
+    
+    #pragma omp parallel for num_threads(NUM_THR)
+    for(i = 0;i < ARR_SIZE; i++)
+       arr[i] = rand()%ARR_SIZE; 
+    
+    iTime = omp_get_wtime();
+    countsort(&arr, ARR_SIZE, 1);
+    fTime = omp_get_wtime() - iTime;
+    //for(i = 0; i<ARR_SIZE; i++) printf("%d, ", arr[i]);
+    printf("Time to sort in Serial: %f\n", fTime);
+
+
+
+
     return 0;
 }
 
-void countsort(int a[], int n) {
-    int i, j, count;
-    int∗ temp = malloc(n∗sizeof(int));
-    for (i = 0; i < n; i++) {
-    count = 0;
-    for (j = 0; j < n; j++)
-        if (a[j] < a[i]) count++;
-        else if (a[j] == a[i] && j < i) count++;
-        temp[count] = a[i];
-    }
-    memcpy(a, temp, n∗sizeof(int));
-    free(temp);
+void countsort(int a[], int n, int num_thr)
+{
+    int i,j,count=0;
+    int *tmp = malloc(sizeof(int) * n);
+    #pragma omp parallel for num_threads(num_thr) private(j) reduction(+:count)
     for(i = 0; i < n; i++)
-        print("%d, ",a[i]);
+    {
+        count = 0;
+        for(j = 0; j < n; j++)
+        {
+            if(a[j] < a[i])
+                    count++;
+            else
+            {
+                if((a[j] == a[i]) && (j < i)) 
+                    count++;
+            }
+        }
+        tmp[count] = a[i];
+    }
+    #pragma omp barrier
+
+    #pragma omp parallel for num_threads(num_thr)
+    for(i = 0; i<n; i++)
+        memcpy(&a[i], &tmp[i], sizeof(int));
+    free(tmp);
 }
